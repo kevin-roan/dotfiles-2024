23873d87c5e42b84416306abf64c65e958531772
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2024-02-26 19:19:19 +0530

    Added InstallScript

---
+++ install.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+echo "Welcome to LudanDWM Install Script"
+echo "You will be prompted to enter sudo password, do enter password when prompted"
+cd ./local/src/
+ls
+cd dmenu
+sudo make clean install
+cd ../dwm
+sudo make clean install
+cd ../dwmblocks
+sudo make clean install
+cd ../slock
+sudo make clean install
+cd ../st
+sudo make clean install
+pwd 
+cd ../.. 
+mv ./config ~/.config
