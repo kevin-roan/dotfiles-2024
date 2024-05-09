5241af4fa9065ae4705cd9444a1ffc2d4c40f3ee
Harshit Tiwari <105152083+iamharshit188@users.noreply.github.com>
2024-05-09 08:29:33 +0530

    pacman: added new packages
    Minimal dependency to render DWM in place.

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 echo "Welcome to LudanDWM Install Script"
 echo "You will be prompted to enter sudo password, do enter password when prompted"
+#Added Minimal dependencies for LudanDWM
+sudo pacman -S libx11 libxft imagemagick feh libxinerama xorg-server xorg-xinit ttf-jetbrains-mono noto-fonts python-pip trash-cli --noconfirm 
+## Installing Pywal for Wallpaper color support
+pip install pywal --break
 ls
 cd dmenu
 sudo make clean install
